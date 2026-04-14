03925442130599cdb8581fa836bc441b5220e221
rxceed <m.risang25@gmail.com>
2026-04-14 21:34:15 +0700

    feat: added .env example

--- src/index.ts
+++ src/index.ts
@@ -2,6 +2,7 @@ import { serve } from "bun";
 import index from "./index.html";
 
 const server = serve({
+  port: process.env.APP_PORT,
   routes: {
     // Serve index.html for all unmatched routes.
     "/*": index,
